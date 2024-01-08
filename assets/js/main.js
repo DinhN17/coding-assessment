36a37b794fafcf325ebad88825f0c7293766704e
Dinhn17 <nquydinh@yahoo.com>
2024-01-09 00:11:50 +1030

    adding show quiz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 // JS questions source from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
-
 //Object: questionBank contains jsQuestions, methods: generateQuiz, 
 const questionBank = {
     //jsQuestions: an array contain all of questions for javascript. Each element of array contains question, options, and answer.
@@ -71,7 +70,6 @@ const questionBank = {
         return retArray;
     },
 
-
     // generateJSQuiz return an array of given number of random questions from jsQuestion.
     generateJSQuiz: function(number) {
         // var srcJSQuiz = this.jsQuestions;
@@ -85,3 +83,102 @@ const questionBank = {
         return retJSQuiz;
     },
 }
+
+// HTML elements
+var viewHighscores = document.getElementById("view-highscores");
+var timeEL = document.getElementById("timer");
+var quizEL = document.getElementById("quiz");
+var quizHeader = document.getElementById("quiz-header");
+var quizBody = document.getElementById("quiz-body");
+var quizFooter = document.getElementById("quiz-footer");
+var startButton = document.getElementById("start-quiz");
+
+// Global constants and variables
+const numberQuestions = 5;
+
+var questionPatch;
+var questionIndex = 0;
+var lastAnswer = "";
+var timeCounter = 100;
+
+// render question in quiz.
+function renderQuiz(question) {
+    // render question on quiz-header
+    quizHeader.firstElementChild.textContent = question.question;
+    
+    // render options on quiz-body
+    quizBody.innerHTML = "";
+    var ul = document.createElement("ul");
+    quizBody.appendChild(ul);
+
+    question.options.forEach(element => {
+        var li = document.createElement("li");
+        li.textContent = element;
+        
+        ul.appendChild(li);                
+    });
+    
+    // render last result on quiz-footer
+    quizFooter.innerHTML = "";
+    if (lastAnswer !== "") {   
+        var p = document.createElement("p");
+        p.textContent = lastAnswer;
+        quizFooter.appendChild(p);     
+    };
+
+}
+
+// render submit result
+function renderSubmitResult() {
+    // render question on quiz-header
+    quizHeader.firstElementChild.textContent = "All done!";
+    
+    // render final scores on quiz-body
+    quizBody.innerHTML = "";
+    var p = document.createElement("p");
+    p.textContent = "Your final score is " + timeCounter;
+    quizBody.appendChild(p);
+    
+    // render Textbox and submit button on quiz-footer
+    quizFooter.innerHTML = "";
+    
+}
+
+function init() {    
+}
+
+// const quiz = questionBank.generateJSQuiz(5);
+// showQuiz(questionBank.generateJSQuiz(5));
+
+//click on Start Quiz button
+startButton.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches("button") === true) {
+        questionPatch = questionBank.generateJSQuiz(numberQuestions);
+        //render quiz
+        questionIndex = 0;
+        renderQuiz(questionPatch[questionIndex]);
+
+        //start timer
+
+      }    
+})
+
+//click on option in quiz-body to make choice
+quizBody.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches("li") === true) {
+        //render quiz
+        questionIndex++;
+        if (questionIndex < questionPatch.length) {
+            renderQuiz(questionPatch[questionIndex]);            
+        } else {
+            renderSubmitResult();
+        };
+        
+      }    
+})
+
+//click on View Highscores
+
+init();
